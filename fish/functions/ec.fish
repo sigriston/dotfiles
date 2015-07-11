58dce54aa1ef3acbff7e477624fc46f0571fa12b
Thiago Sigrist <sigrist@gmail.com>
2015-07-11 02:01:14 -0300

    removed whitespace

--- fish/functions/ec.fish
+++ fish/functions/ec.fish
@@ -3,7 +3,7 @@
 function ec
   if pgrep -q Emacs
     emacsclient -n $argv[1]
-  else 
+  else
     set -l filePath (pwd)'/'$argv[1]
     which osascript > /dev/null ^&1; and \
       osascript -e 'tell application "Emacs"' \
